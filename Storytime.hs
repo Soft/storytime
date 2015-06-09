441ed116025b6bbc30c4fe087920a2cd0ca93592
Samuel Laurén <samuel.lauren@iki.fi>
2015-06-09 14:40:43 +0300

    Validation

--- Storytime.hs
+++ Storytime.hs
@@ -4,5 +4,6 @@ import Storytime.Types as S
 import Storytime.Monadic as S
 import Storytime.Parsing as S
 import Storytime.Evaluation as S
+import Storytime.Validate as S
 import Storytime.Terminal as S
 
