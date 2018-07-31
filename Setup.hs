7ed8c47c7513d1023c2ecbe374a441aa135e8245
Samuel Laurén <samuel.lauren@iki.fi>
2018-07-31 20:03:47 +0300

    Install dependencies managed by npm if missing

--- Setup.hs
+++ Setup.hs
@@ -1,21 +1,36 @@
+import Control.Monad (unless)
 import Distribution.Simple
 import Distribution.Simple.Setup (BuildFlags)
 import Distribution.Types.HookedBuildInfo (HookedBuildInfo, emptyHookedBuildInfo)
 import System.Exit (ExitCode(..))
+import System.FilePath ((</>))
+import System.Directory (doesDirectoryExist)
 import System.Process (CreateProcess(..), proc, withCreateProcess, waitForProcess)
+
+npmProc :: [String] -> String -> String -> IO ()
+npmProc args success error = do
+  let npmProc = (proc "npm" args) { cwd = Just "frontend" }
+  withCreateProcess npmProc $ \_ _ _ handle -> do
+    status <- waitForProcess handle
+    case status of
+      ExitSuccess -> putStrLn success
+      ExitFailure code -> putStrLn $ error ++ " (exit code: " ++ show code ++ ")"
+
+npmInstall :: IO ()
+npmInstall = do
+  putStrLn "Installing frontend dependencies"
+  npmProc ["install"] "Dependencies installed" "Failed to install dependencies"
 
 buildFrontend :: IO ()
 buildFrontend = do
   putStrLn "Building frontend"
-  let npmProc = (proc "npm" ["run", "build"]) { cwd = Just "frontend" }
-  withCreateProcess npmProc $ \_ _ _ handle -> do
-    status <- waitForProcess handle
-    case status of
-      ExitSuccess -> putStrLn "Frontend compiled"
-      ExitFailure code -> putStrLn $ "Failed to build frontend (exit code: " ++ show code ++ ")"
+  npmProc ["run", "build"] "Fronted compiled" "Failed to build frontend"
 
 preBuildHook :: Args -> BuildFlags -> IO HookedBuildInfo
-preBuildHook args flags = buildFrontend >>
+preBuildHook args flags = do
+  modulesDir <- doesDirectoryExist $ "frontend" </> "node_modules"
+  unless modulesDir npmInstall
+  buildFrontend
   return emptyHookedBuildInfo
 
 main :: IO ()
